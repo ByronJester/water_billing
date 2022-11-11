46abeeb0136c769a3475e93897db6ebfba0b3047
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-11 18:01:20 +0800

    commit

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 echo "Running composer"
+composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
@@ -8,4 +10,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --forces

0326ba56d4d731dfa32db3b6782ae3da43f34ec7
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-11 10:27:17 +0800

    Commit

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,23 +1,9 @@
-#!/bin/sh
-
-if [ -e .env ]
-then
-    echo "Exporting env variables..."
-    set -a
-    . ./.env
-    set +a
-else
-    echo ".env not found. Skipping..."
-fi
-
-cd "$KEY_PATH" && ls -la
-chmod -R 755 "$KEY_PATH" && ls -la
-cd /var/www/html
-
-# composer run-script post-autoload-dump
-# https://laravel.com/docs/7.x/deployment#optimizing-configuration-loading
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
 php artisan config:cache
 php artisan route:cache
 # php artisan cache:clear
 php artisan migrate --force
+

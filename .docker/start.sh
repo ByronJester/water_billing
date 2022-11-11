f0787d72a08f57efc4c25e16f00c32213184a7bd
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-11 17:30:18 +0800

    commit

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-if [ -e .env ]
-then
-    echo "Exporting env variables..."
-    set -a
-    . ./.env
-    set +a
-else
-    echo ".env not found. Skipping..."
-fi
-
-cd "$KEY_PATH" && ls -la
-chmod -R 755 "$KEY_PATH" && ls -la
-cd /var/www/html
-
 # composer run-script post-autoload-dump
 # https://laravel.com/docs/7.x/deployment#optimizing-configuration-loading
 

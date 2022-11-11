e4e1b8f21a8545258aaa2436d167dbe4c2143a39
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-11 11:55:10 +0800

    m

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,19 +1,7 @@
-if [ -e .env ]
-then
-    echo "Exporting env variables..."
-    set -a
-    . ./.env
-    set +a
-else
-    echo ".env not found. Skipping..."
-fi
-
-cd "$KEY_PATH" && ls -la
-chmod -R 755 "$KEY_PATH" && ls -la
-cd /var/www/html
-
 php artisan config:cache
 php artisan route:cache
 # php artisan cache:clear
 php artisan migrate --force
+
+apache2ctl -D FOREGROUND
 

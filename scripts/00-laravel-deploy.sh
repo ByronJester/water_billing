5fa42e1f122551ddcdb98528059a25ff56091431
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-13 14:40:53 +0800

    Commit

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --no-scripts --optimize-autoloader --no-autoloader --no-ansi --no-interaction -no-progress --no-dev --profile --working-dir=/var/www/html
-composer dump-autoload --optimize --classmap-authoritative --no-interaction --no-scripts --no-dev
+composer install --no-dev --working-dir=/var/www/html
+composer dump-autoload --no-dev
 
 echo "Caching config..."
 php artisan config:cache

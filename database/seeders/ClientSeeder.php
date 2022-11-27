01c8a2c5291fe2c08a07c386cac00d15400c0ff8
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-11-27 16:56:10 +0800

    m

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Client;
 use App\Models\ClientPayment;
+use Illuminate\Support\Str;
 
 class ClientSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class ClientSeeder extends Seeder
      */
     public function run()
     {
-        
+        Client::create([
+            'first_name' => 'Jessa',
+            'middle_name' => 'Palabrica',
+            'last_name' => 'Ruedas',
+            'address' => 'Catandaan, Nasugbu, Batangas',
+            'phone' => '09657657443',
+            'reference' => strtoupper(Str::random(8))
+        ]);
     }
 }
